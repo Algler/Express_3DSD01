5e14ceeffc82b379698826f4fcc84018bd81c9d0
ALLAN BUENO DA SILVA <130380709+Algler@users.noreply.github.com>
2024-10-04 11:55:12 +0000

    criou rotas e começou o menu

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express();
 
-const PORT = 3333;
+const PORT = 3331;
+
+// Criação das rotas
+
+app.use(express.static(__dirname + "public"));
 
 app.get("/", function (req, res) {
-  res.send("joy soi un atacante");
+  res.sendFile(__dirname + "/src/views/index.html");
+});
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/src/views/about.html");
+});
+
+// Criar rota 404 (MIDDLEWARE)
+
+app.use(function (req, res) {
+  res.status(404).sendFile(__dirname + "/src/views/404.html");
 });
 
 app.listen(PORT, () => {
